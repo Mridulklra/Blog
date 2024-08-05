60b0907a0ec917426dd170fec4a80c64365004d4
Mridul Kalra <mridulkalra700@gmail.com>
2024-08-06 01:55:57 +0530

    backend is working

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'; 
+dotenv.config();
 const app=express();
+
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  app.use('/api/user',userRoutes);
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
-  
+ 
+   
